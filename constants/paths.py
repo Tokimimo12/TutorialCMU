229e0f7adfa57a20f810938ade0a5cb904d8810f
Tokimimo12 <simion.nicoleta612@gmail.com>
2024-12-09 21:16:29 +0100

    What has been done until now without data

---
+++ constants/paths.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+# path to the SDK folder
+SDK_PATH: Optional[str] = 'C:\\Users\\Viki\\Documents\\Thesis\\CMU-MultimodalSDK'
+print("SDK_PATH:", SDK_PATH)
+
+# path to the folder where you want to store data
+DATA_PATH: Optional[str] = 'C:\\Users\\Viki\\Documents\\Thesis\\tryout3\\data'
+
+# path to a pretrained word embedding file
+WORD_EMB_PATH: Optional[str] = 'C:\\Users\\Viki\\Documents\\Thesis\\tryout3\\data\\http__immortal.multicomp.cs.cmu.edu\\CMU-MOSI\\language\\CMU_MOSI_TimestampedWordVectors.csd'
+
+# path to loaded word embedding matrix and corresponding word2id mapping
+CACHE_PATH: Optional[str] = 'C:\\Users\\Viki\\Documents\\Thesis\\tryout3\\data\\embedding_and_mapping.pt'
+
